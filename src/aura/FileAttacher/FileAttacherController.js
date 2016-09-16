5738eb333bf9f89142131c0ffc326b27bd4dfcc7
Dave Scruggs <davescruggs84@gmail.com>
2016-09-16 19:27:34 -0400

    Updating to upload files

--- src/aura/FileAttacher/FileAttacherController.js
+++ src/aura/FileAttacher/FileAttacherController.js
@@ -1,4 +1,35 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+    onInit: function(component) {
+		component.set("v.pictures", []);
+    },
+
+    onDragOver: function(component, event) {
+        event.preventDefault();
+    },
+
+    onDrop: function(component, event, helper) {
+
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+
+        var files = event.dataTransfer.files;
+
+        for (var i=0; i<files.length; i=i+1) {
+            var file = files[i];
+            if (file.type.match(/(image.*)/)) {
+                var reader = new FileReader();
+                reader.onloadend = function(e) {
+                    var dataURL = e.target.result;
+                    var pictures = component.get("v.pictures");
+                    pictures.push(dataURL);
+                    component.set("v.pictures", pictures);
+                    helper.upload(component, file, dataURL.match(/,(.*)$/)[1]);
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+
+    }
+
 })
--- src/aura/FileAttacher/FileAttacherHelper.js
+++ src/aura/FileAttacher/FileAttacherHelper.js
@@ -1,4 +1,50 @@
 ({
-	helperMethod : function() {
-	}
+    upload: function(component, file, base64Data) {
+        var action = component.get("c.saveAttachment");
+        action.setParams({
+            parentId: component.get("v.recordId"),
+            fileName: file.name,
+            base64Data: base64Data,
+            contentType: file.type
+        });
+				console.log("saving " + file.name + " of type " + file.type + " to record " + component.get("v.recordId") );
+				console.log("===== start file data ===== ");
+				console.log(base64Data);
+				console.log("=====  end file data  ===== ");
+				action.setCallback(this, function(a) {
+						var state = a.getState();
+						console.log("in upload callback method");
+            //component.set("v.message", state==="SUCCESS"?"Image uploaded":"Upload error");
+						console.log("upload callback returned " + state);
+						if (state === "SUCCESS") {
+                // Alert the user with the value returned
+                // from the server
+                console.log("Upload callback returned from server: " + a.getReturnValue());
+								component.set("v.message", "Image uploaded " + a.getReturnValue());
+                // You would typically fire a event here to trigger
+                // client-side notification that the server-side
+                // action is complete
+            }
+            //else if (cmp.isValid() && state === "INCOMPLETE") {
+            else if (state === "INCOMPLETE") {
+                // do something
+								console.log("status in upload callback is incomplete");
+            }
+            //else if (cmp.isValid() && state === "ERROR") {
+            else if (state === "ERROR") {
+                var errors = a.getError();
+								component.set("v.message", "Upload Error " + errors);
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+        component.set("v.message", "Uploading...");
+    }
 })
